14cf97328306ee8c97a35935d8a3dcd566034fe7
amishasinghh <53701356+amishasinghh@users.noreply.github.com>
2021-07-26 00:12:26 +0530

    Add files via upload

---
+++ autocomplete.js
@@ -0,0 +1,12 @@
+/*Add the JavaScript here for the function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */
+function billingFunction(){
+    SName = document.getElementById('shippingName').value;
+    SZip = document.getElementById('shippingZip').value;
+    if(document.getElementById('same').checked){
+    document.getElementById('billingName').value = SName;
+    document.getElementById('billingZip').value = SZip;}
+    else{
+      document.getElementById('billingName').value = '';
+    document.getElementById('billingZip').value = '';
+    }
+}
